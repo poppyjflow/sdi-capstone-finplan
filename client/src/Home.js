028f207da0cf8f23440e6a6e3a6b459189acc626
Jason Flowers <poppyjflow@gmail.com>
2022-12-07 14:45:26 -0800

    vertical hide sort-of working

--- client/src/Home.js
+++ client/src/Home.js
@@ -3,15 +3,16 @@ import ReactDataGrid from '@inovua/reactdatagrid-enterprise'
 import '@inovua/reactdatagrid-enterprise/index.css'
 import CheckBox from '@inovua/reactdatagrid-community/packages/CheckBox'
 import '@inovua/reactdatagrid-enterprise/theme/default-dark.css'
-// import NotificationBoard from '@inovua/react-toolkit/Notification'
-// import '@inovua/react-toolkit/Notification/index.css'
 
 const Home = () => {
-//  const [showQ1Columns, setShowQ1Columns] = useState('false');
-  const [visibleQ1, setVisibleQ1] = useState(false)
+const [visibleQ1, setVisibleQ1] = useState(false)
+const [visibleQ2, setVisibleQ2] = useState(false)
+const [visibleQ3, setVisibleQ3] = useState(false)
+const [visibleQ4, setVisibleQ4] = useState(false)
 
-  const getColumns = ({ visibleQ1 }) => {
-    return [
+//  const getColumns = () => {
+  const getColumns =
+    [
       { name: 'id', header: 'ID', minWidth: 50, defaultFlex: 2 },
       { name: 'pri_ranking', header: 'Pri Ranking', maxWidth: 1000, defaultFlex: 1 },
       // { name: 'wing_id', header: 'Wing', maxWidth: 1000, defaultFlex: 1 },
@@ -23,30 +24,32 @@ const Home = () => {
       // { name: 'desc_title', header: 'Description', maxWidth: 1000, defaultFlex: 1 },
       { name: 'desc_details', header: 'Details', maxWidth: 1000, defaultFlex: 1 },
       { name: 'desc_impact', header: 'Impact', maxWidth: 1000, defaultFlex: 1 },
-      { name: 'q1_desc', header: 'Q1 Desc', maxWidth: 1000, defaultFlex: 1, visibleQ1 },
-      { name: 'q1_requested', header: 'Q1 Requested', maxWidth: 1000, defaultFlex: 1, visibleQ1 },
-      { name: 'q1_allocated', header: 'Q1 Allocated', maxWidth: 1000, defaultFlex: 1, visibleQ1 },
-      { name: 'q1_obligated', header: 'Q1 Obligated', maxWidth: 1000, defaultFlex: 1, visibleQ1 }
-      // { name: 'q2_desc', header: 'Q2 Desc', maxWidth: 1000, defaultFlex: 1 },
-      // { name: 'q2_requested', header: 'Q2 Requested', maxWidth: 1000, defaultFlex: 1 },
-      // { name: 'q2_allocated', header: 'Q2 Allocated', maxWidth: 1000, defaultFlex: 1 },
-      // { name: 'q2_obligated', header: 'Q2 Obligated', maxWidth: 1000, defaultFlex: 1 },
-      // { name: 'q3_desc', header: 'Q3 Desc', maxWidth: 1000, defaultFlex: 1 },
-      // { name: 'q3_requested', header: 'Q3 Requested', maxWidth: 1000, defaultFlex: 1 },
-      // { name: 'q3_allocated', header: 'Q3 Allocated', maxWidth: 1000, defaultFlex: 1 },
-      // { name: 'q3_obligated', header: 'Q3 Obligated', maxWidth: 1000, defaultFlex: 1 },
-      // { name: 'q4_desc', header: 'Q4 Desc', maxWidth: 1000, defaultFlex: 1 },
-      // { name: 'q4_requested', header: 'Q4 Requested', maxWidth: 1000, defaultFlex: 1 },
-      // { name: 'q4_allocated', header: 'Q4 Allocated', maxWidth: 1000, defaultFlex: 1 },
-      // { name: 'q4_obligated', header: 'Q4 Obligated', maxWidth: 1000, defaultFlex: 1 },
+      // { name: 'q1_desc', header: 'Q1 Desc', width: -1, defaultFlex: 1, hidden: true },
+      { name: 'q1_desc', header: 'Q1 Desc', maxWidth: 1000, defaultFlex: 1, visible: visibleQ1 },
+      { name: 'q1_requested', header: 'Q1 Requested', maxWidth: 1000, defaultFlex: 1, visible: visibleQ1 },
+      { name: 'q1_allocated', header: 'Q1 Allocated', maxWidth: 1000, defaultFlex: 1, visible: visibleQ1 },
+      { name: 'q1_obligated', header: 'Q1 Obligated', maxWidth: 1000, defaultFlex: 1, visible: visibleQ1 },
+      { name: 'q2_desc', header: 'Q2 Desc', maxWidth: 1000, defaultFlex: 1, visible: visibleQ2 },
+      { name: 'q2_requested', header: 'Q2 Requested', maxWidth: 1000, defaultFlex: 1, visible: visibleQ2 },
+      { name: 'q2_allocated', header: 'Q2 Allocated', maxWidth: 1000, defaultFlex: 1, visible: visibleQ2 },
+      { name: 'q2_obligated', header: 'Q2 Obligated', maxWidth: 1000, defaultFlex: 1, visible: visibleQ2 },
+      { name: 'q3_desc', header: 'Q3 Desc', maxWidth: 1000, defaultFlex: 1, visible: visibleQ3 },
+      { name: 'q3_requested', header: 'Q3 Requested', maxWidth: 1000, defaultFlex: 1, visible: visibleQ3 },
+      { name: 'q3_allocated', header: 'Q3 Allocated', maxWidth: 1000, defaultFlex: 1, visible: visibleQ3 },
+      { name: 'q3_obligated', header: 'Q3 Obligated', maxWidth: 1000, defaultFlex: 1, visible: visibleQ3 },
+      { name: 'q4_desc', header: 'Q4 Desc', maxWidth: 1000, defaultFlex: 1, visible: visibleQ4 },
+      { name: 'q4_requested', header: 'Q4 Requested', maxWidth: 1000, defaultFlex: 1, visible: visibleQ4 },
+      { name: 'q4_allocated', header: 'Q4 Allocated', maxWidth: 1000, defaultFlex: 1, visible: visibleQ4 },
+      { name: 'q4_obligated', header: 'Q4 Obligated', maxWidth: 1000, defaultFlex: 1, visible: visibleQ4 },
     ]
-  }
+//  }
 
-  const [columns, setColumns] = useState(getColumns({ visibleQ1 }))
+//  const [columns, setColumns] = useState(getColumns())
+  const [columns, setColumns] = useState(getColumns)
   const gridStyle = { minHeight: 550 }
 
   const dataSource = [
-    { id: 1, pri_ranking: 'John McQueen', age: 35 },
+    { id: 1, pri_ranking: 'John McQueen', desc_details: 'desc_details', desc_impact: 'desc_impact', q1_desc: 'q1_desc', q1_requested: 'q1_requested' },
     { id: 2, pri_ranking: 'Mary Stones', age: 25 },
     { id: 3, pri_ranking: 'Robert Fil', age: 27 },
     { id: 4, pri_ranking: 'Roger Robson', age: 81 },
@@ -85,12 +88,12 @@ const Home = () => {
     ]//)
   }, [])
 
-  const onColumnVisibleChange = useCallback(({ column, visible }) => {
+//  const onColumnVisibleChange = useCallback(({ column, visible }) => {
     // inovua.notification.first.addNotification({
     //   title: 'Visibility changed',
     //   content: column.name + ' column visibility is set to: ' + visible
     // })
-  }, [])
+//  }, [])
 
 
   //FETCH MISSION DATA
@@ -126,15 +129,14 @@ const Home = () => {
 console.log(`render`)
   return (
     <>
-       <CheckBox
-          checked={visibleQ1}
-          onChange={visibleQ1 => {
-            setVisibleQ1(visibleQ1)
-            setColumns(getColumns({ visibleQ1 }))
+       <CheckBox checked={visibleQ1} onChange={clickVal => {
+console.log(`clickVal: ${clickVal}`)
+            setVisibleQ1(clickVal)
+            return setTimeout(() => {console.log("timeout");setColumns(getColumns)}, 500);
+
           }}
-        >
-          Show Id column
-      </CheckBox>
+          >Show Q1
+       </CheckBox>
 
       <ReactDataGrid
         idProperty="id"
@@ -142,34 +144,31 @@ console.log(`render`)
         dataSource={dataSource}
         style={gridStyle}
         renderColumnContextMenu={renderColumnContextMenu}
-        onColumnVisibleChange={onColumnVisibleChange}
+//        onColumnVisibleChange={onColumnVisibleChange}
       />
     </>
     )
-//   }
 }
 
 export default Home
 
-
 /*
-    <Card border='light' style={{ width: '18rem' }} key='1' bg='dark' text='white'className="mission-card">
-      <Card.Body className='card-body'>
-        <Card.Text>
-          {movieSearchData.map(movie => {
-            return <div key={movie.id}>{movie.title}</div>
-          })}
-        </Card.Text>
-      </Card.Body>
-    </Card>
-
-    <div className="mainsearch">
-      <input
-          className="text-search-bar"
-          type='text'
-          placeholder="Search Movies"
-          onChange={(event) => {handleSearch(event)}}
-          value={searchTerm}
-      />
-  </div>
+       <CheckBox checked={visibleQ2} onChange={visibleQ2 => {
+            setVisibleQ2(visibleQ2)
+            setColumns(getColumns())
+          }}
+          >Show Q2
+       </CheckBox>
+       <CheckBox checked={visibleQ3} onChange={visibleQ3 => {
+            setVisibleQ3(visibleQ3)
+            setColumns(getColumns())
+          }}
+          >Show Q3
+       </CheckBox>
+       <CheckBox checked={visibleQ4} onChange={visibleQ4 => {
+            setVisibleQ4(visibleQ4)
+            setColumns(getColumns())
+          }}
+          >Show Q4
+       </CheckBox>
 */
