4e078460d12c1e1ad884afc91a1aa1095bb4358c
SonOfRobin <MDRobins@proton.me>
2022-12-13 10:00:03 -0500

    Seed DB

--- api/db/migrations/20221212222710_create_users.js
+++ api/db/migrations/20221212222710_create_users.js
@@ -6,7 +6,7 @@ exports.up = function (knex) {
   return knex.schema.createTable('users', table => {
     table.increments('id');
     table.integer('org').references('id').inTable('orgs').onDelete('CASCADE');
-    table.integer('rank');
+    table.string('rank');
     table.string('l_name').notNullable;
     table.string('f_name').notNullable;
     table.string('password').notNullable();
---
+++ api/db/seeds/00_seed_users.js
@@ -0,0 +1,13 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> } 
+ */
+
+
+exports.seed = async function (knex) {
+  // Deletes ALL existing entries
+  await knex('users').del();
+  await knex('users').insert(
+
+  );
+};
