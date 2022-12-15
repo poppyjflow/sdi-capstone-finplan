36cddf1a336563f78d9081cbc088292e41277e24
Kevin Wang <lol@noemail.com>
2022-12-15 16:59:14 -0500

    knexfile change

--- api/src/email.js
+++ api/src/email.js
@@ -1,18 +1,43 @@
+const {getRequest, getWithID, deleteRequest, getID} = require('./queryHelpers.js')
 const express = require("express");
 const nodemailer = require("nodemailer");
+const env = process.env.NODE_ENV || 'development'
+const config = require('../knexfile')[env]
+
+const knex = require('knex')(config)
 const app = express();
 const port = 5555;
 
 
+app.use(express.json());
+
 function periodicRequest() {
-    setInterval(function() {
-      // Make the request to the backend server here
-    }, 50000); // Make the request every 5 seconds
+    setInterval( async function() {
+        let selectedInfo = [];
+
+         await knex('users')
+            .select('id','email','org')
+            // .where('id','=','1')
+            .then(info => {
+                console.log(info)
+                return info
+            })
+            .then(info => selectedInfo = info)
+            .catch(err =>console.log(err))
+        
+        for (let user of selectedInfo){
+            console.log("setting options")
+            let option = mailOptionSetter(user.email)
+
+            console.log('sending email')
+            sendMail(option)
+        }
+        
+    }, 30000); //5 sec
   }
 
+periodicRequest();
 
-app.use(express.json());
-// require("dotenv").config();
 
 let transporter = nodemailer.createTransport({
     host: "smtp.mailtrap.io",
@@ -33,10 +58,25 @@ function mailOptionSetter(endUserEmail) {
     return mailOptions
 }
 
+function sendMail(options){
+    transporter.sendMail(options, (error, info) => {
+        // console.log(error)
+        if (error) {
+            res.status(400).json("error: ",`${error}`)
+            return console.log(error);
+        }
+        console.log('Message sent: %s', info.messageId)
+        res.status(200).json(`${info.messageId}`);
+    })
 
+}
+
+// send "instant" email
 app.post('/sendemail', (req, res) => {
     const emailEndUser = req.body.endUserEmail;
     console.log(emailEndUser);
+
+
     let options = mailOptionSetter(emailEndUser);
 
     console.log(options);
@@ -49,10 +89,9 @@ app.post('/sendemail', (req, res) => {
         }
         console.log('Message sent: %s', info.messageId)
         res.status(200).json(`${info.messageId}`);
-    });
+    })
+});
 
-
-})
 
 
 
