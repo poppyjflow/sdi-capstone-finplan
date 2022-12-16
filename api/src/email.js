d61d9d1673f14db50812ce80e2741c4c686c0a43
Kevin Wang <lol@noemail.com>
2022-12-16 11:04:18 -0500

    save1

--- api/src/email.js
+++ api/src/email.js
@@ -11,6 +11,7 @@ const port = 5555;
 
 app.use(express.json());
 
+//periodic  email knex and sent;
 function periodicRequest() {
     setInterval( async function() {
         let selectedInfo = [];
@@ -26,19 +27,20 @@ function periodicRequest() {
             .catch(err =>console.log(err))
         
         for (let user of selectedInfo){
-            console.log("setting options")
+            // console.log("setting options")
             let option = mailOptionSetter(user.email)
 
-            console.log('sending email')
+            // console.log('sending email')
             sendMail(option)
         }
         
     }, 30000); //5 sec
   }
 
+// execute the function 
 periodicRequest();
 
-
+// authication to transport to the mailtrap io
 let transporter = nodemailer.createTransport({
     host: "smtp.mailtrap.io",
     port: 2525,
@@ -47,6 +49,8 @@ let transporter = nodemailer.createTransport({
         pass: "701762a8ca2d3d"
     }
 });
+
+// set text and email address
 function mailOptionSetter(endUserEmail) {
     let mailOptions = {
         from: '"System Admin" <from@example.com>',
@@ -58,29 +62,22 @@ function mailOptionSetter(endUserEmail) {
     return mailOptions
 }
 
+// sent the mail 
 function sendMail(options){
     transporter.sendMail(options, (error, info) => {
         // console.log(error)
         if (error) {
-            res.status(400).json("error: ",`${error}`)
+            // res.status(400).json("error: ",`${error}`)
             return console.log(error);
         }
         console.log('Message sent: %s', info.messageId)
-        res.status(200).json(`${info.messageId}`);
+        // res.status(200).json(`${info.messageId}`);
     })
 
 }
 
-// send "instant" email
-app.post('/sendemail', (req, res) => {
-    const emailEndUser = req.body.endUserEmail;
-    console.log(emailEndUser);
-
-
-    let options = mailOptionSetter(emailEndUser);
-
-    console.log(options);
-
+//sent the mail with respond to the backend
+function sendMailInstant(options,res){
     transporter.sendMail(options, (error, info) => {
         // console.log(error)
         if (error) {
@@ -88,8 +85,18 @@ app.post('/sendemail', (req, res) => {
             return console.log(error);
         }
         console.log('Message sent: %s', info.messageId)
-        res.status(200).json(`${info.messageId}`);
+        return res.status(200).json(`${info.messageId}`);
     })
+
+}
+
+// send "instant" email endpoint 
+app.post('/sendemail', (req, res) => {
+    const emailEndUser = req.body.endUserEmail;
+    console.log(emailEndUser);
+    let options = mailOptionSetter(emailEndUser);
+    console.log(options);
+    sendMailInstant(options,res);
 });
 
 
