4b340716ef1e88272b37d44f46468de952dc508c
tyleroverholts <tyoverholts@gmail.com>
2022-12-19 08:06:42 -0700

    Pre master merge; added summary page; readding knexfile

--- client/src/routes/SummaryView.jsx
+++ client/src/routes/SummaryView.jsx
@@ -10,19 +10,25 @@ import { QuarterlyBarChart } from '../components/QuarterlyBarChart';
 const SummaryView = () => {
     return(
     <Grid sx={{flexGrow: 1, maxWidth: '100%', maxHeight: '100%', marginTop: '1em', marginBottom:'1em'}}>
-      <Grid container spacing={3}>
-        <Grid item xs={12}>
-        <Card sx={{width: '100%', height: '35%', marginBottom: '1em'}}>
+      <Grid container
+      spacing={3}
+      justifyContent="space-between"
+      sx={{marginBottom: '5em'}}
+      >
+        <Grid item xs={2}>
+        <Card sx={{width: '12.5em', height: '4em'}}>
           <CardContent>
           <Typography gutterBottom variant="h5" component="div">
             Summary View
           </Typography>
           </CardContent>
         </Card>
-          <Card sx={{width: '100%', height: '45%'}}>
+        </Grid>
+        <Grid item xs={2}>
+          <Card sx={{width: '15em', height: '5em'}}>
           <CardContent>
             <Typography gutterBottom variant="h8" component="div" >
-              Current Quarter: Q4, 2022
+              Current Quarter: Q1, 2023
             </Typography>
             <Typography gutterBottom variant="h9" component="div">
               Next Due Date: 30 Dec 2022
@@ -30,6 +36,8 @@ const SummaryView = () => {
           </CardContent>
           </Card>
         </Grid>
+        </Grid>
+        <Grid container spacing={3}>
         <Grid item xs={8} sx={{marginBottom: '1em'}}>
           <QuarterlyBarChart />
         </Grid>
